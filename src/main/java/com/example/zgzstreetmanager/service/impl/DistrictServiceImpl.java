c6309aa3034db224e9c009dc6509b05365136e20
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-09-22 16:05:21 +0200

    agregar json con la zonificacion completa

--- src/main/java/com/example/zgzstreetmanager/service/impl/DistrictServiceImpl.java
+++ src/main/java/com/example/zgzstreetmanager/service/impl/DistrictServiceImpl.java
@@ -24,36 +24,6 @@ public class DistrictServiceImpl implements DistrictService {
     private  static final Map<String, Path2D> DISTRICT_POLYGONS = new HashMap<>();
 
 
-
-
-
-    //            Path2D polygon = new Path2D.Double();
-//            polygon.moveTo(41.659406501547835, -0.8816846325500194);
-//            polygon.lineTo(41.667257606264286, -0.8803551565459031);
-//            polygon.lineTo(41.67688083416155, -0.8802018608488993);
-//            polygon.lineTo(41.67952625554304, -0.8780860825566492);
-//            polygon.lineTo(41.67973784395028, -0.865973768300222);
-//            polygon.lineTo(41.67816518951233, -0.8444517787498569);
-//            polygon.lineTo(41.68271892106762, -0.8462831025178446);
-//            polygon.lineTo(41.68610331619357, -0.843810680292421);
-//            polygon.lineTo(41.69186263168004, -0.8587935786136711);
-//            polygon.lineTo(41.68927240561581, -0.8638458535109237);
-//            polygon.lineTo(41.6929739933079, -0.8697403812373068);
-//            polygon.lineTo(41.69002733578236, -0.8869319691426085);
-//            polygon.lineTo(41.68787204768978, -0.8864956554945138);
-//            polygon.lineTo(41.687731253349625, -0.8907247923686441);
-//            polygon.lineTo(41.68962222467559, -0.9005128971057559);
-//            polygon.lineTo(41.680437971610274, -0.8994972084822734);
-//            polygon.lineTo(41.6745974721606, -0.9051884289043528);
-//            polygon.lineTo(41.6716374830838, -0.9160759630670725);
-//            polygon.lineTo(41.66613511871, -0.9201631887612223);
-//            polygon.lineTo(41.666496454995325, -0.908432797139568);
-//            polygon.lineTo(41.66779435154015, -0.902474018715111);
-//            polygon.lineTo(41.66680380087976, -0.8964694704026783);
-//            polygon.lineTo(41.66129928997009, -0.8889317049963097);
-//            polygon.closePath();
-
-
     public static List<GeoJSONObject.Feature> extractFeatures(String jsonData) {
         ObjectMapper objectMapper = new ObjectMapper();
         List<GeoJSONObject.Feature> features = new ArrayList<>();
@@ -72,45 +42,6 @@ public class DistrictServiceImpl implements DistrictService {
     }
 
 
-//    public static List<List<Double>> extractCoordinates(String jsonData) {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode root;
-//
-//        try {
-//            root = objectMapper.readTree(jsonData);
-//
-//            JsonNode coordinatesNode = root.path("features").path(0).path("geometry").path("coordinates").path(0);
-//
-//            List<List<Double>> coords = new ArrayList<>();
-//            for (int i = 0; i < coordinatesNode.size(); i++) {
-//                JsonNode coordinatePair = coordinatesNode.path(i);
-//                List<Double> coordinate = new ArrayList<>();
-//                coordinate.add(coordinatePair.path(0).asDouble());
-//                coordinate.add(coordinatePair.path(1).asDouble());
-//                coords.add(coordinate);
-//            }
-//            return coords;
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public static DistrictEnum extractDistrictEnum(String jsonData){
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode root;
-//
-//        try {
-//            root = objectMapper.readTree(jsonData);
-//
-//            JsonNode districtNode = root.path("features").path(0).path("name");
-//            return DistrictEnum.valueOf(districtNode.asText());
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
     private static void createDistrictPolygon(GeoJSONObject.Feature feature) {
         Path2D polygon = new Path2D.Double();
         List<List<List<Double>>> coordinates = feature.getGeometry().getCoordinates();
@@ -126,7 +57,6 @@ public class DistrictServiceImpl implements DistrictService {
         polygon.closePath();
 
         DISTRICT_POLYGONS.put(feature.getProperties().getName(), polygon);
-        System.out.println("Distrito " + feature.getProperties().getName() + " creado.");
     }
 
 
